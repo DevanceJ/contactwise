94e409bbef8df34db8e16cf1b2e183ada1d44c9c
devance <dj788@snu.edu.in>
2024-07-31 11:32:21 +0530

    shifted to nodemailer

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,14 +1,69 @@
-import { Resend } from "resend";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const url = process.env.NEXT_PUBLIC_URL;
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const url = process.env.NEXT_PUBLIC_URL;
+// export const sendVerificationEmail = async (email: string, token: string) => {
+//   const verificationURL = `${url}/auth/verify-email?token=${token}`;
+
+//   await resend.emails.send({
+//     from: "onboarding@resend.dev",
+//     to: email,
+//     subject: "Verify your email",
+//     html: `
+//         <h1>Verify your email</h1>
+//         <p>Click the link below to verify your email address.</p>
+//         <a href="${verificationURL}">Verify email</a>
+//     `,
+//   });
+// };
+
+// export const sendPasswordResetEmail = async (email: string, token: string) => {
+//   const passwordResetURL = `${url}/auth/reset-password?token=${token}`;
+
+//   await resend.emails.send({
+//     from: "onboarding@resend.dev",
+//     to: email,
+//     subject: "Reset your password",
+//     html: `
+//         <h1>Reset your password</h1>
+//         <p>Click the link below to reset your password.</p>
+//         <a href="${passwordResetURL}">Reset password</a>
+//     `,
+//   });
+// };
+
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    // user: process.env.EMAIL,
+    user: "noch.email.contact@gmail.com",
+    pass: process.env.PASSWORD,
+  },
+});
+
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const verificationURL = `${url}/auth/verify-email?token=${token}`;
+  const verificationURL = `${process.env.NEXT_PUBLIC_URL}/auth/verify-email?token=${token}`;
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  await new Promise((resolve, reject) => {
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready");
+        resolve(success);
+      }
+    });
+  });
+  await transporter.sendMail({
+    from: "noch.email.contact@gmail.com",
     to: email,
-    subject: "Verify your email",
+    subject: "Noch: Verify your email",
     html: `
         <h1>Verify your email</h1>
         <p>Click the link below to verify your email address.</p>
@@ -18,12 +73,23 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const passwordResetURL = `${url}/auth/reset-password?token=${token}`;
+  const passwordResetURL = `${process.env.NEXT_PUBLIC_URL}/auth/reset-password?token=${token}`;
 
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
+  await new Promise((resolve, reject) => {
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready");
+        resolve(success);
+      }
+    });
+  });
+  await transporter.sendMail({
+    from: "noch.email.contact@gmail.com",
     to: email,
-    subject: "Reset your password",
+    subject: "Noch: Reset your password",
     html: `
         <h1>Reset your password</h1>
         <p>Click the link below to reset your password.</p>
